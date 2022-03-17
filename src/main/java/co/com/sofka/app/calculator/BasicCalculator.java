c60c5723a4789a3724006ce97c82198d86320f05
Diego Gonzalez <degonzalez6@gmail.com>
2022-03-16 22:55:02 -0300

    Se agrega la clase BasicCalculatorTest ejecutando funciones de casos de prueba

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -1,2 +1,33 @@
-package co.com.sofka.app.calculator;public class BasicCalculator {
+package co.com.sofka.app.calculator;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2){
+        logger.info("Summing {} + {}", number1, number2);
+        return number1 + number2;
+    }
+
+    public Long subs(Long number1, Long number2){
+        logger.info("Substracting {} - {}", number1, number2);
+        return number1 - number2;
+    }
+
+    public Long mult(Long number1, Long number2){
+        logger.info("Multiplying {} * {}", number1, number2);
+        return number1 * number2;
+    }
+
+    public Long div(Long number1, Long number2){
+        logger.info("Dividing {} / {}", number1, number2);
+        if(number2==0){
+            throw new IllegalArgumentException("No se puede dividir entre cero");
+        }
+        return number1 + number2;
+    }
 }
